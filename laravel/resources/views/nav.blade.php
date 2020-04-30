0647ea388c3b1c3cf682c5d25134bca3b301c53a
tabiojisann <takumiezumi@icloud.com>
2020-05-01 00:56:16 +0900

    ログイン画面

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -4,25 +4,25 @@
 
   <ul class="navbar-nav ml-auto">
 
-   
+    @guest 
     <li class="nav-item">
       <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a> {{--この行を変更--}}
     </li>
-   
+    @endguest 
 
-   
+    @guest 
     <li class="nav-item">
-      <a class="nav-link" href="">ログイン</a>
+      <a class="nav-link" href="{{ route('login') }}">ログイン</a>
     </li>
-   
+    @endguest 
       
-   
+    @auth 
     <li class="nav-item">
       <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
+    @endauth 
     
-    
-    
+    @auth 
     <!-- Dropdown -->
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
@@ -40,11 +40,11 @@
         </button>
       </div>
     </li>
-    <form id="logout-button" method="POST" action="{{ route('logout') }}"> 
-      @csrf
+    <form id="logout-button" method="POST" action="{{ route('logout') }}"> {{--この行を編集--}}
+      @csrf 
     </form>
     <!-- Dropdown -->
-   
+    @endauth 
 
   </ul>
 
