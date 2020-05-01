41710acc1d13a04d629c5226fa786c9b85d106c3
tabiojisann <takumiezumi@icloud.com>
2020-05-01 15:15:44 +0900

    記事更新、削除、詳細

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -10,7 +10,12 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    
+
+     public function __construct()
+     {
+         $this->authorizeResource(Article::class, 'article');
+     }
+     
      public function index()
      {
          $articles = Article::all()->sortByDesc('created_at');
@@ -31,7 +36,28 @@ class ArticleController extends Controller
          $article->save();
          return redirect()->route('articles.index');
      }
+
+     public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);    
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
    
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
+    }    
 }
 
 
