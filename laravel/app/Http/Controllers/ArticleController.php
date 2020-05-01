341dcd3d7170fd43f062cac143ec10cd13dbe93f
tabiojisann <takumiezumi@icloud.com>
2020-05-01 14:35:03 +0900

    投稿OK

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Article;
 
+use App\Http\Requests\ArticleRequest;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -15,5 +17,21 @@ class ArticleController extends Controller
         
          return view('articles.index', ['articles' => $articles]);
      }
+
+     public function create()
+     {
+         return view('articles.create');    
+     }
+
+     public function store(ArticleRequest $request, Article $article)
+     {
+     
+         $article->fill($request->all());
+         $article->user_id = $request->user()->id;
+         $article->save();
+         return redirect()->route('articles.index');
+     }
    
 }
+
+
