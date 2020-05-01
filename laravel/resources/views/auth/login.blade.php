5ce1f9d389cacfa4da7ce19fa567657c568221f7
tabiojisann <takumiezumi@icloud.com>
2020-05-01 20:19:51 +0900

    Fixed
    修正

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -34,9 +34,7 @@
                   <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
                 </div>
 
-
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
-
               </form>
 
               <div class="mt-0">
