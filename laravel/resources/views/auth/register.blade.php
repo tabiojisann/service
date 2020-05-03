ac2b5d1ce5a15fc3df32afbd7e6538a8e28cf88c
tabiojisann <59763220+tabiojisann@users.noreply.github.com>
2020-05-03 18:54:09 +0900

    登録フォーム画面
    少しコードがみづらいので

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -21,23 +21,28 @@
    
               <form method="POST" action="{{ route('register') }}">
                 @csrf
+                
                 <div class="md-form">
                   <label for="name">ユーザー名</label>
                   <input class="form-control" type="text" id="name" name="name" required value="{{ old('name') }}">
                   <small>英数字3〜16文字(登録後の変更はできません)</small>
                 </div>
+                
                 <div class="md-form">
                   <label for="email">メールアドレス</label>
                   <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}" >
                 </div>
+                
                 <div class="md-form">
                   <label for="password">パスワード</label>
                   <input class="form-control" type="password" id="password" name="password" required>
                 </div>
+                
                 <div class="md-form">
                   <label for="password_confirmation">パスワード(確認)</label>
                   <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
                 </div>
+                
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
               </form>
           
