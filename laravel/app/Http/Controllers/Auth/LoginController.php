4132826ea51aec27f52946ee1b069e475953ea84
tabiojisann <takumiezumi@icloud.com>
2020-05-03 17:14:15 +0900

    c

--- laravel/app/Http/Controllers/Auth/LoginController.php
+++ laravel/app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Laravel\Socialite\Facades\Socialite;
+
 class LoginController extends Controller
 {
     /*
@@ -36,5 +38,10 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToProvider(string $provider)
+    {
+        return Socialite::driver($provider)->redirect();
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,7 +11,13 @@
 |
 */
 
-Auth::routes();
+// Auth::routes();
+
+Route::prefix('login')->name('login.')->group(function () {
+  Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+  Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
+});
+
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
