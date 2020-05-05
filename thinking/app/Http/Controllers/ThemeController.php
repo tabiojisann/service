206e32f9db6773595d623ace05121b6399739e1e
tabiojisann <takumiezumi@icloud.com>
2020-05-05 21:41:51 +0900

    コントローラー修正

--- thinking/app/Http/Controllers/ThemeController.php
+++ thinking/app/Http/Controllers/ThemeController.php
@@ -2,41 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use \App\Theme;
+
 use Illuminate\Http\Request;
 
 class ThemeController extends Controller
 {
     public function index() {
-        
-        $themes = [
-            (object) [
-                'id' => 1,
-                'body' => "お題",
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'しほりん',
-                ],
-            ],
-            (object) [
-                'id' => 2,
-                'body' => "お題2",
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'パパ',
-                ],
-            ],
-            (object) [
-                'id' => 3,
-                'body' => "お題3",
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ママ',
-                ],
-            ],
-        ];
+
+        $themes = Theme::all()->sortByDesc('created_at');
 
         return view('themes.index', ['themes' => $themes]);
         }
