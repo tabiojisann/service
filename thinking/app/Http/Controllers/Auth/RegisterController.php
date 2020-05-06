43a709dc60a2d3f37388e2ec70e3c32c3cfefc6e
tabiojisann <takumiezumi@icloud.com>
2020-05-06 16:53:16 +0900

    fix

--- thinking/app/Http/Controllers/Auth/RegisterController.php
+++ thinking/app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'alpha_num', 'min:3', 'max:16', 'unique:users'], 
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'alpha_num', 'min:1', 'max:10', 'unique:users'], 
+            'email' => ['required', 'string', 'email', 'max:100', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
