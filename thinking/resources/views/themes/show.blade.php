32d91a16c199c68c803b7e5ff0f9a5e17799594c
tabiojisann <takumiezumi@icloud.com>
2020-05-08 21:48:51 +0900

    画像ファイルパス一部変更

--- thinking/resources/views/themes/show.blade.php
+++ thinking/resources/views/themes/show.blade.php
@@ -4,7 +4,10 @@
 
 @section('content')
   @include('nav')
-  <div class="container">
+  <div class="card mt-3">
     @include('themes.card')
+      <div class="card-image pt-0">
+        <img src ="{{ str_replace('public/', '/storage/', $theme->image) }}">
+      </div>
   </div>
 @endsection
