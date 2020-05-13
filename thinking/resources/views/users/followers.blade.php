c0eb642524ca6282878ade7bde4bb6b86f8629dc
tabiojisann <takumiezumi@icloud.com>
2020-05-13 10:17:02 +0900

    ui/ux ログイン情報付与

--- thinking/resources/views/users/followers.blade.php
+++ thinking/resources/views/users/followers.blade.php
@@ -3,12 +3,14 @@
 @section('title', $user->name . 'のフォロワー')
 
 @section('content')
-  @include('nav')
-  <div class="container">
-    @include('users.user')
-    @include('users.tabs', ['hasThemes' => false, 'hasLikes' => false])
-    @foreach($followers as $person)
-      @include('users.person')
-    @endforeach
-  </div>
+  <div class="view" style="background-image: url('https://cdn.pixabay.com/photo/2016/01/06/21/42/light-bulbs-1125016_1280.jpg'); background-attachment: fixed; background-position: center center; height: 100vh;">
+    @include('nav')
+    <div class="container">
+      @include('users.user')
+      @include('users.tabs', ['hasThemes' => false, 'hasLikes' => false])
+      @foreach($followers as $person)
+        @include('users.person')
+      @endforeach
+    </div>
+  </div>  
 @endsection
